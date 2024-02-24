233b2e8895fa4cf62633433236240731de74c9bf
KuryKat <57152799+KuryKat@users.noreply.github.com>
2024-02-23 21:01:29 -0300

    Update newRedditRedirect.user.js

--- newRedditRedirect.user.js
+++ newRedditRedirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        New Reddit Redirect
-// @version     1.0.0
+// @version     1.0.1
 // @description Redirects you to new.reddit.com
 // @author      github.com/KuryKat
 // @match       *://*.reddit.com/*
